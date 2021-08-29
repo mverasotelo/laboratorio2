adbd67f8efc77a4c06f3a9ed704f38db9bf98c9b
mverasotelo <mer-vs@hotmail.com>
2021-08-29 19:36:51 -0300

    Ejercicio A01

---
+++ Clase_02 - Ejercicio_A01/Biblioteca/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+
+        /// <summary>
+        /// Calcula el factorial de un numero entero positivo
+        /// </summary>
+        /// <param name="operando"></param>
+        /// <returns>Devuelve factorial si el operando es positivo y 0 si el operando es negativo</returns>
+        public static long CalcularFactorial(int operando)
+        {
+            long factorial = 1;
+            if (operando > 1)
+            {
+                factorial = operando * CalcularFactorial(operando - 1);
+            }
+            else if (operando < 0)
+            {
+                factorial = 0;
+            }
+            return factorial;
+        }   
+    }
+}
---
+++ Clase_02 - Ejercicio_A01/Clase_02 - Ejercicio_A01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_A01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            long resultado;
+
+            Console.Write("Ingrese operando: ");
+            while (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.Write("Eso no es un numero. Ingrese operando: ");
+            }
+
+            resultado = Calculadora.CalcularFactorial(numero);
+
+            if (resultado == 0)
+            {
+                Console.WriteLine("No se puede calcular el factorial de un numero negativo");
+            }
+            else
+            {
+                Console.WriteLine($"El factorial de {numero} es {resultado}");
+            }
+        }
+    }
+}
