d2a55de6a9647b04cfc6fa55ebb8ede8ee9d32b2
mverasotelo <mer-vs@hotmail.com>
2021-08-29 19:07:11 -0300

    Clase 2 - Ejercicio 8

---
+++ Clase_02 - Ejercicio_08/Biblioteca/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        /// <summary>
+        ///  Calcula los dias pasados desde una fecha pasada por parametro hasta la fecha actual   
+        /// </summary>
+        /// <param name="fecha">fecha</param>
+        /// <returns>cantidad de dias pasados</returns>
+        public static int CalcularDias(DateTime fecha)
+        {
+            return (DateTime.Now - fecha).Days;
+        }
+    }
+}
---
+++ Clase_02 - Ejercicio_08/Clase_02 - Ejercicio_08/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime fecha = new DateTime();
+
+            Console.Write("Ingrese la fecha de su cumpleaños (dd/mm/aaaa): ");
+
+            while (!DateTime.TryParse(Console.ReadLine(), out fecha))
+            {
+                Console.Write("Esa no es una fecha. Ingrese la fecha de su cumpleaños (dd/mm/aaaa): ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"Usted ha vivido {Calculadora.CalcularDias(fecha)} dias");
+        }
+    }
+}
