0fcd8a885c11a9942a8adecb6447d10529616fd1
mverasotelo <mer-vs@hotmail.com>
2021-08-29 17:43:31 -0300

    git add .

---
+++ Clase_02 - Ejercicio_07/Biblioteca/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Calcula la hipotenusa de un triangulo rectangulo, siguiendo el teorema de Pitagoras
+        /// </summary>
+        /// <param name="baseTriangulo">Base del triangulo en centimetros</param>
+        /// <param name="alturaTriangulo">Altura del triangulo en centimetros</param>
+        /// <returns>Hipotenusa en centimetros</returns>
+        public static double CalcularHipotenusa(double baseTriangulo, double alturaTriangulo)
+        {
+            return Math.Sqrt(Math.Pow(baseTriangulo, 2) + Math.Pow(alturaTriangulo, 2));
+        }
+    }
+}
---
+++ Clase_02 - Ejercicio_07/Clase_02 - Ejercicio_07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_02___Ejercicio_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float baseTriangulo;
+            float alturaTriangulo;
+
+            Console.Write("Ingrese la base del triangulo en cm.: ");
+
+            while (!float.TryParse(Console.ReadLine(), out baseTriangulo))
+            {
+                Console.Write("Eso no es un numero. Ingrese la base del triangulo en cm.: ");
+            }
+
+            Console.Write("Ingrese la altura del triangulo en cm.: ");
+
+            while (!float.TryParse(Console.ReadLine(), out alturaTriangulo))
+            {
+                Console.Write("Eso no es un numero. Ingrese la altura del triangulo en cm.: ");
+            }
+
+            Console.WriteLine("La hipotenusa del triangulo ingresado es: {0:N2} cm",Calculadora.CalcularHipotenusa(baseTriangulo, alturaTriangulo));
+        }
+    }
+}
