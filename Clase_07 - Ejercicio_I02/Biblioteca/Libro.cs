93b4cbf58674814dd58bcfdbb85c8851d2253760
mverasotelo <mer-vs@hotmail.com>
2021-09-13 22:45:30 -0300

    Ejercicio2-Clase7

---
+++ Clase_07 - Ejercicio_I02/Biblioteca/Libro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            paginas = new List<string>();
+        }
+        public List<string> Paginas
+        {
+            get
+            {
+                return paginas;
+            }
+        }
+
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < paginas.Count)
+                { 
+                    return paginas[index];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            set
+            {
+                if (paginas is not null && index >= 0 && index < paginas.Count)
+                {
+                     paginas[index] = value;
+                }
+                else
+                {
+                    paginas.Add(value);
+                }
+            }
+        }
+    }
+}
---
+++ Clase_07 - Ejercicio_I02/Clase_07 - Ejercicio_I02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Biblioteca;
+
+namespace Clase_07___Ejercicio_I02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            libro[28] = "Hola";
+            libro[18] = "Buenos dias";
+            libro[3] = "Chau";
+
+            Console.WriteLine(libro[2]);
+
+            libro[2] = "Chau pisado";
+            libro[95] = "Hasta mañana";
+
+            Console.WriteLine(libro[2]);
+
+            Console.WriteLine("___________");
+            foreach (string s in libro.Paginas)
+            {
+                Console.WriteLine($"{s}");
+            }
+        }
+    }
+}
